63bc73f14be3806f9d17422a058a904b3d6f1630
joselerma97 <josemanuel@lermaserver.com>
2023-11-01 22:45:34 +0100

    Software Architecture

--- app/PredictionsController.py
+++ app/PredictionsController.py
@@ -1,10 +1,11 @@
 from app.GeneralView import GeneralView
-
+from app.PredictionsModel import PredictionsModel
 
 class PredictionsController:
 
-    def __init__(self, view: GeneralView):
+    def __init__(self, view: GeneralView, model: PredictionsModel):
         self._view = view
+        self._model = model
 
     def show(self, container):
         self._view.init_container(container)
--- virtual_world.py
+++ virtual_world.py
@@ -4,16 +4,27 @@ from app.MenuController import MenuController
 from app.EDAController import EDAController
 from app.PredictionsController import PredictionsController
 from app.EDAModel import EDAModel
+from app.PredictionsModel import PredictionsModel
 
+# Creation of the App
 if __name__ == "__main__":
+    # Create GUI elements
     view = GeneralView()
 
+    # Create Plotly Charts
     eda_model = EDAModel()
+    # Manage GUI Elements and Charts
     eda_controller = EDAController(view, eda_model)
 
-    predictions_controller = PredictionsController(view)
+    # Create ML Objects
+    predictions_model = PredictionsModel()
+    # Manage GUI Elements and ML operations
+    predictions_controller = PredictionsController(view, predictions_model)
 
+    # Create GUI Menu and open screens
     menu_view = MenuView(eda_controller, predictions_controller)
+    # Manage Menu Operations
     menu_controller = MenuController(menu_view)
 
+    # Init App
     menu_controller.init_gui()
