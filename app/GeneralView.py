71d66b03653b184894cb1de8c63d57ac02f980ad
joselerma97 <josemanuel@lermaserver.com>
2023-11-06 02:55:42 +0100

    Updating view

--- app/GeneralView.py
+++ app/GeneralView.py
@@ -3,44 +3,63 @@ import streamlit as st
 class GeneralView:
 
     def __init__(self):
+        self._top_container = None
         self._main_container = None
+        self._bottom_container = None
 
-    def init_container(self, general_container: st):
+    def init_container(self, general_container: st, columns_num=1):
         with general_container.container():
-            self._main_container = st.container()
+            with st.container():
+                self._top_container = st.container()
+                self._main_container = st.columns(columns_num)
+                self._bottom_container = st.container()
 
-    def write_titles(self, title: str, instructions: str):
-        with self._main_container:
+    def write_titles(self, title: str, instructions: str, num_column=0):
+        with self._main_container[num_column]:
             st.title(title)
             st.header(instructions)
 
-    def write_number_input(self, name):
-        with self._main_container:
+    def write_titles_top(self, title: str, instructions: str):
+        with self._top_container:
+            st.title(title)
+            if instructions != "":
+                st.header(instructions)
+
+    def write_number_input(self, name, num_column=0):
+        with self._main_container[num_column]:
             value = st.number_input(name, min_value=0.0, step=1.0)
         return value
 
-    def write_selector(self, label, start, *input_names):
+    def write_selector(self, label, start, num_column=0, *input_names):
         options = dict()
         for index, name in enumerate(input_names):
             options[name] = index + start
-        with self._main_container:
+        with self._main_container[num_column]:
             selected_label = st.selectbox(label, options.keys())
             value_return = options[selected_label]
         return value_return
 
-    def plotly_show(self, fig):
-        with self._main_container:
+    def plotly_show(self, fig, num_column=0):
+        with self._main_container[num_column]:
             st.plotly_chart(fig, use_container_width=True)
 
-    def write_text(self, text):
-        with self._main_container:
+    def write_text(self, text, num_column=0):
+        with self._main_container[num_column]:
             st.write(text)
 
-    def write_slider(self, label, start, finish, step):
-        with self._main_container:
+    def write_text_bottom(self, text):
+        with self._bottom_container:
+            st.write(text)
+
+    def write_slider(self, label, start, finish, step, num_column=0):
+        with self._main_container[num_column]:
             value = st.slider(label, start, finish, step)
         return value
 
-    def write_header(self, text):
-        with self._main_container:
+    def write_header(self, text, num_column=0):
+        with self._main_container[num_column]:
+            st.header(text)
+
+    def write_header_top(self, text):
+        with self._top_container:
             st.header(text)
--- app/simulator/SimulatorController.py
+++ app/simulator/SimulatorController.py
@@ -12,39 +12,50 @@ class SimulatorController:
                                        0.0, 0.0, 0.0, 0.0)
 
     def show(self, container):
-        self._view.init_container(container)
-        self._view.write_titles("Simulator", "Fill all the values")
+        self._view.init_container(container, 3)
+        self._view.write_titles_top(
+            "Simulator",
+            ""
+        )
         self._bikes_input.season = self._view.write_selector(
-            "Season", 1, "Springer", "Summer", "Fall", "Winter"
+            "Season", 1, 0, "Springer", "Summer", "Fall", "Winter"
         )
         self._bikes_input.day = self._view.write_slider("Day", 1, 31, 1)
         self._bikes_input.hr = self._view.write_slider("Hour", 0, 23, 1)
         self._bikes_input.holiday = self._view.write_selector(
-            "Holiday", 0, "No", "Yes"
+            "Holiday", 0, 0, "No", "Yes"
         )
         self._bikes_input.weekday = self._view.write_selector(
-            "Weekday", 0, "Monday", "Tuesday",
+            "Weekday", 0, 1, "Monday", "Tuesday",
             "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
         )
         self._bikes_input.workingday = self._view.write_selector(
-            "Workingday", 0, "No", "Yes"
+            "Workingday", 0, 1, "No", "Yes"
         )
         self._bikes_input.weathersit = self._view.write_selector(
-            "Weather Type", 1, "Clear, Few clouds, Partly cloudy, Partly cloudy",
+            "Weather Type", 1, 1, "Clear, Few clouds, Partly cloudy, Partly cloudy",
             "Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist",
             "Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds",
             "Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog"
         )
-        self._bikes_input.temp = self._view.write_number_input("Normalized temperature in Celsius. The values are divided to 41 (max)")
-        self._bikes_input.hum = self._view.write_number_input("Normalized humidity. The values are divided to 100 (max)")
-        self._bikes_input.windspeed = self._view.write_number_input("Normalized wind speed. The values are divided to 67 (max)")
-        self._bikes_input.daylight_hours = self._view.write_number_input("Daylight Hours")
+        self._bikes_input.temp = self._view.write_number_input(
+            "Normalized temperature in Celsius. The values are divided to 41 (max)", 1
+        )
+        self._bikes_input.hum = self._view.write_number_input(
+            "Normalized humidity. The values are divided to 100 (max)", 2
+        )
+        self._bikes_input.windspeed = self._view.write_number_input(
+            "Normalized wind speed. The values are divided to 67 (max)", 2
+        )
+        self._bikes_input.daylight_hours = self._view.write_number_input(
+            "Daylight Hours", 2
+        )
 
-        self._view.write_text("Median History Weather Values")
-        self._view.write_text(self._model.get_median_conditions(
+        self._view.write_text_bottom("Median History Weather Values")
+        self._view.write_text_bottom(self._model.get_median_conditions(
             self._bikes_input.season, self._bikes_input.weathersit
         ))
 
-        self._view.write_header(
+        self._view.write_header_top(
             f"Bikes Count Prediction: {self._model.predict_cnt(self._bikes_input)}"
         )
